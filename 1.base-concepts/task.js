4448ff4ccb0d95ddd4e50fe2feaf0e57574015a5
angelos <angelinapisklova65@gmail.com>
2026-03-11 10:00:21 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
+  const discriminant = b ** - 4 * a * c;
   let arr = [];
-  
+
+  if (discriminant < 0) {
+    return [];
+    } else if (discriminant === 0) {
+    const x = -b / (2 * a);
+    return [Math.round(x)];
+  } else {
+    const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
   return arr;
 }
 
