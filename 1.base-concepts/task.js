9ba4c01f64b51af8f5cde2796559d5719b2dbd2b
angelos <angelinapisklova65@gmail.com>
2026-03-11 10:30:50 +0300

    Реализовано второго задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,6 @@
 "use strict"
 function solveEquation(a, b, c) {
-  const discriminant = b ** - 4 * a * c;
+  const discriminant = b ** 2 - 4 * a * c;
   let arr = [];
 
   if (discriminant < 0) {
@@ -11,9 +11,22 @@ function solveEquation(a, b, c) {
   } else {
     const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-  return arr;
+  arr.push(x1, x2);
+    return arr;
+}
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = (percent / 100) / 12;
+  let s = amount - contribution;
+
+  if (s <= 0) {
+    return 0;
+  }
+
+  let monthlyPayment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
